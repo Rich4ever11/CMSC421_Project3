44072b5363cb38d8b277bb40ac242e730d81b9bd
Richard <ml60379@umbc.edu>
2022-11-24 13:38:31 -0500

    completion of project3 part1

--- part1/insperation.c
+++ part1/insperation.c
@@ -1,40 +1,104 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/random.h>
+#include <linux/uaccess.h>
+#include <linux/fs.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
  
+static const char *quotes[] = {
+	"\"Spread love everywhere you go. Let no one ever come to you without leaving happier.\" - Mother Teresa",
+	"\"When you reach the end of your rope, tie a knot in it and hang on.\" - Franklin D. Roosevelt",
+	"\"Always remember that you are absolutely unique. Just like everyone else.\" - Margaret Mead",
+	"\"Don't judge each day by the harvest you reap but by the seeds that you plant.\" - Robert Louis Stevenson",
+	"\"The future belongs to those who believe in the beauty of their dreams.\" - Eleanor Roosevelt",
+	"\"Tell me and I forget. Teach me and I remember. Involve me and I learn.\" - Benjamin Franklin",
+	"\"The best and most beautiful things in the world cannot be seen or even touched — they must be felt with the heart.\" - Helen Keller",
+	"\"It is during our darkest moments that we must focus to see the light.\" - Aristotle",
+	"\"Whoever is happy will make others happy too.\" - Anne Frank",
+	"\"Do not go where the path may lead, go instead where there is no path and 	leave a trail.\" - Ralph Waldo Emerson",
+	"\"The only place where your dream becomes impossible is in your own thinking.\" - Robert H Schuller",
+	"\"Happiness often sneaks in through a door you didn’t know you left open.\" - John Barrymore",
+	"\"He who does not understand your silence will probably not understand your words.\" - Elbert Hubbard",
+	"\"To live is the rarest thing in the world. Most people exist, that is all.\" - Oscar Wilde",
+	"\"To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.\" - Ralph Waldo Emerson",
+	"\"If you look at what you have in life, you’ll always have more.\" - Oprah Winfrey",
+	"\"A champion is defined not by their wins but by how they can recover when they fall.\" - Serena Williams",
+	"\"If my life is going to mean anything, I have to live it myself.\" - Rick Riordan",
+	"\"Start where you are. Use what you have. Do what you can.\" - Arthur Ashe",
+	"\"It does not matter how slowly you go, so long as you do not stop.\" - Confucius"
+};
+
+/*Device read functionality: ex. cat /dev/insperation */
+static ssize_t device_read(struct file *file, char __user *user_buffer, size_t size, loff_t *offset)
+{
+    /*Assigned Variables*/
+    unsigned randomNum;
+    ssize_t len;
+    
+    /*gets the random number*/
+    get_random_bytes(&randomNum, sizeof randomNum);
+    randomNum = (1u + (randomNum % 20u)) - 1u;
+    /*gets the length of the randomly selected quote*/
+    len = strlen(quotes[randomNum]);
+    
+    /*Adds the quote to the buffer*/
+    return simple_read_from_buffer(user_buffer, size, offset, quotes[randomNum], len);
+}
+
+/*Device write functionality: ex. echo "hi" > /dev/insperation */
+static ssize_t device_write(struct file *f, const char __user *buf, size_t len, loff_t *off)
+{
+    /*Print that this operation is not programmed*/
+    printk(KERN_ALERT "Sorry, This operation is not supported.\n");
+    /*returns appropriate error*/
+    return EPERM;
+}
+
+/*Declaration of the file_operations*/
+/*User can only currently read and write*/
+static struct file_operations fops = {
+	.read = device_read,
+	.write = device_write
+};
+
+/*Sets the permissions so that all users can read and write*/
+static int set_permissions(struct device *dev, struct kobj_uevent_env *env)
+{
+    add_uevent_var(env, "DEVMODE=%#o", 0666);
+    return 0;
+}
+
+/*Variable Declarations*/
+static struct cdev char_dev;
+static dev_t first = 0;
+static struct class *device_class; 
+
 MODULE_LICENSE("GPL");
  
 static int __init qoute_generator(void) {
-    unsigned randomNum;
-    get_random_bytes(&randomNum, sizeof randomNum);
-    randomNum = (1u + (randomNum % 20u)) - 1u;
-    static const char *quotes[20];
-    quotes[0] = "\"Spread love everywhere you go. Let no one ever come to you without leaving happier.\" - Mother Teresa";
-    quotes[1] = "\"When you reach the end of your rope, tie a knot in it and hang on.\" - Franklin D. Roosevelt";
-    quotes[2] = "\"Always remember that you are absolutely unique. Just like everyone else.\" - Margaret Mead";
-    quotes[3] = "\"Don't judge each day by the harvest you reap but by the seeds that you plant.\" - Robert Louis Stevenson";
-    quotes[4] = "\"The future belongs to those who believe in the beauty of their dreams.\" - Eleanor Roosevelt";
-    quotes[5] = "\"Tell me and I forget. Teach me and I remember. Involve me and I learn.\" - Benjamin Franklin";
-    quotes[6] = "\"The best and most beautiful things in the world cannot be seen or even touched — they must be felt with the heart.\" - Helen Keller";
-    quotes[7] = "\"It is during our darkest moments that we must focus to see the light.\" - Aristotle";
-    quotes[8] = "\"Whoever is happy will make others happy too.\" - Anne Frank";
-    quotes[9] = "\"Do not go where the path may lead, go instead where there is no path and leave a trail.\" - Ralph Waldo Emerson";
-    quotes[10] = "\"The only place where your dream becomes impossible is in your own thinking.\" - Robert H Schuller";
-    quotes[11] = "\"Happiness often sneaks in through a door you didn’t know you left open.\" - John Barrymore";
-    quotes[12] = "\"He who does not understand your silence will probably not understand your words.\" - Elbert Hubbard";
-    quotes[13] = "\"To live is the rarest thing in the world. Most people exist, that is all.\" - Oscar Wilde";
-    quotes[14] = "\"To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.\" - Ralph Waldo Emerson";
-    quotes[15] = "\"If you look at what you have in life, you’ll always have more.\" - Oprah Winfrey";
-    quotes[16] = "\"A champion is defined not by their wins but by how they can recover when they fall.\" - Serena Williams";
-    quotes[17] = "\"If my life is going to mean anything, I have to live it myself.\" - Rick Riordan";
-    quotes[18] = "\"Start where you are. Use what you have. Do what you can.\" - Arthur Ashe";
-    quotes[19] = "\"It does not matter how slowly you go, so long as you do not stop.\" - Confucius";
-    
-    printk(KERN_ALERT "%s\n", quotes[randomNum]);
+    /*registers a range of char device numbers*/
+    alloc_chrdev_region(&first, 0, 3, "insperation");
+    /*creation of virtual device class*/
+    device_class = class_create(THIS_MODULE, "insperation");
+    /*Set the permission so that it can be read and written too*/
+    device_class->dev_uevent = set_permissions;
+    /*Create the actual device and creates the sysfs attribute file for the device*/
+    device_create(device_class, NULL, first, NULL, "insperation");
+    /*initialize the cdev structure, adds file operations*/
+    cdev_init(&char_dev, &fops);
+    /*add char device to the system*/
+    cdev_add(&char_dev, first, 1);
+
     return 0;
 }
  
 static void __exit qoute_exit(void) {
+    /*unregisters an destroys device (Basically clean up of all the resources)*/
+    unregister_chrdev_region(first, 3);
+    device_destroy(device_class, first);
+    class_destroy(device_class);
+    unregister_chrdev_region(first, 1);
     printk(KERN_ALERT "Thank you for using the quote generator\n");
 }
  
--- part1/insperation.mod.c
+++ part1/insperation.mod.c
@@ -28,10 +28,21 @@ MODULE_INFO(retpoline, "Y");
 static const struct modversion_info ____versions[]
 __used __section("__versions") = {
 	{ 0xe28fa882, "module_layout" },
-	{ 0x5b8239ca, "__x86_return_thunk" },
+	{ 0x48106fee, "class_destroy" },
+	{ 0x3d598bde, "device_destroy" },
+	{ 0x6091b333, "unregister_chrdev_region" },
+	{ 0x37d92150, "cdev_add" },
+	{ 0x65f84319, "cdev_init" },
+	{ 0xb10dd8d4, "device_create" },
+	{ 0xcee0cc87, "__class_create" },
+	{ 0xe3ec2f2b, "alloc_chrdev_region" },
+	{ 0x9c6febfc, "add_uevent_var" },
 	{ 0xd0da656b, "__stack_chk_fail" },
-	{ 0x92997ed8, "_printk" },
+	{ 0x619cb7dd, "simple_read_from_buffer" },
+	{ 0x754d539c, "strlen" },
 	{ 0x41ed3709, "get_random_bytes" },
+	{ 0x5b8239ca, "__x86_return_thunk" },
+	{ 0x92997ed8, "_printk" },
 	{ 0xbdfb6dbb, "__fentry__" },
 };
 
